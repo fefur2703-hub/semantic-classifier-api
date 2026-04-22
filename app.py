de9862ef4ddd7245c215df255eb3a4c271774778
fefur2703-hub <fefur2703@gmail.com>
2026-04-22 14:41:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import random
+import sys
+
+sys.path.append("./source")
+from sca_utils import TextClassifier
 
 app = FastAPI()
+
+classifier = TextClassifier(
+    model_multiclass_path="./models/model_02_E.h5",
+    encoder_multiclass_path="./models/encoder_oneHot_E.pickle",
+    model_regression_path="./models/model_01_D2.h5"
+)
 
 class TextInput(BaseModel):
     text: str
@@ -13,11 +22,12 @@ def root():
 
 @app.post("/analyze")
 def analyze(data: TextInput):
-    
-    score = round(random.uniform(0, 1), 2)
+    prob_obj, prob_subj = classifier.textClassifier_getSCA_regression(data.text)
+    sca_label = classifier.textClassifier_getSCA_multiclass(data.text)
 
     return {
         "text": data.text,
-        "subjectivity": score,
-        "classification": "alta" if score > 0.6 else "baixa"
+        "objectivity": float(prob_obj) if prob_obj is not None else None,
+        "subjectivity": float(prob_subj) if prob_subj is not None else None,
+        "sca_label": sca_label
     }
