955e38c6f21e74955a27e1f5be73e7519ed266c0
fefur2703-hub <fefur2703@gmail.com>
2026-04-22 13:56:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TextInput(BaseModel):
+    text: str
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.post("/analyze")
+def analyze(data: TextInput):
+    return {
+        "text": data.text,
+        "message": "API funcionando"
+    }
